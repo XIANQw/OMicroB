fb960ba02e74bdfea3a4ad7e4a00700f5e882e6e
XIANQw <qiwei-xian@qq.com>
2020-05-22 17:22:36 +0800

    add a screan bar

--- src/byterun/client/gui.c
+++ src/byterun/client/gui.c
@@ -139,12 +139,15 @@ inline void shm_init(){
 GtkWidget* create_grid(int col, int row){
     GtkWidget* grid, *button;
     grid = gtk_grid_new();
+    button = gtk_button_new_with_label("Screen");
+    gtk_widget_set_sensitive(button, FALSE);
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 0, col, 1);
     for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
             button = gtk_button_new_with_label("");
             screen[i][j] = button;
             gtk_widget_set_sensitive(button, FALSE);
-            gtk_grid_attach(GTK_GRID(grid), button, i, j, 1, 1);
+            gtk_grid_attach(GTK_GRID(grid), button, i, 1+j, 1, 1);
         }
     }
     return grid;
@@ -160,8 +163,8 @@ GtkWidget* create_UI(){
     
     GtkWidget* box=gtk_box_new(GTK_ORIENTATION_VERTICAL, 3);
     gtk_container_add(GTK_CONTAINER(window), box);
+
     GtkWidget* grid=create_grid(SCREEN_SIZE, SCREEN_SIZE);
-    
     button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
     gtk_box_set_spacing(GTK_BOX(button_box), 5);
     gtk_box_pack_start(GTK_BOX(box), grid, FALSE, FALSE, 3);
